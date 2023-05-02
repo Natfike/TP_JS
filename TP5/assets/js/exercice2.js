46f043010b30e4c811e9f6b5ee858492c5e513af
hain erwan <erwan.hain@etu.univ-cotedazur.fr>
2023-05-02 11:09:23 +0200

    Rajout exercice2

--- TP5/assets/js/exercice2.js
+++ TP5/assets/js/exercice2.js
@@ -1,18 +1,33 @@
-for (let i=0;i<3;i++){
-    $.ajax({
-        url: "https://dummyjson.com/"+i,
-    })
+$('.product-template').hide();
+
+let i;
+
+function addProduit(){
+    if(i<=100){
+        $.ajax({
+            url: "https://dummyjson.com/product/"+i,
+            type: 'GET',
+            success: function(data){
+                console.log("Hello");
+                const produit = $('.product-template').clone();
+                produit.removeClass('product-template');
+                produit.addClass('product-' + i);
+                produit.find('.img').attr('src', data.thumbnail).attr('alt', data.title);
+                produit.find('.title').text(data.title);
+                produit.find('.brand').text(data.brand);
+                produit.find('.price').text(`Price: ${data.price}`);
+                const oldPrice = data.price + data.price / data.discountPercentage;
+                produit.find('.old-price').text(`Old price: ${oldPrice}`);
+                produit.find('.stock').text(`Stock: ${data.stock}`);
+                produit.show();
+                $('body').append(produit);
+            }, error: function(jqXHR, textStatus, errorThrown){
+                console.log('Une erreur est survenue lors de l\'envoir du formulaire ');
+            }
+        })
+        i += 1;
+    }
 }
 
-
-$.ajax({
-    url: 'form.php',
-    type: 'POST',
-    data: {email:email, message: message},
-    success: function(response){
-        console.log(response);
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-        console.log(errorThrown);
-    }
-})
+i = 1;
+document.getElementById("product-generation").addEventListener('click',addProduit);
